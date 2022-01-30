9c1a1896fadd850149bdfb749985c1c3076826a7
iBisho <ibisho.cwaemu@gmail.com>
2022-01-30 18:23:04 -0300

    M4A1

--- lua/weapons/m9k_m4a1/shared.lua
+++ lua/weapons/m9k_m4a1/shared.lua
@@ -36,7 +36,7 @@ SWEP.Kind     = WEAPON_HEAVY
 SWEP.WeaponID = AMMO_MAC10
 
 SWEP.Primary.Sound			= Sound("Dmgfok_M4A1.Single")		
-SWEP.Primary.RPM			= 800			
+SWEP.Primary.RPM			= 950			
 SWEP.Primary.ClipSize		= 30	
 SWEP.Primary.DefaultClip	= 30	
 SWEP.Primary.ClipMax        = 60
@@ -58,7 +58,8 @@ SWEP.data 				= {}
 SWEP.data.ironsights	= 1
 
 SWEP.Primary.NumShots	  = 1		
-SWEP.Primary.Damage		  = 23
+SWEP.Primary.Damage		  = 21
+SWEP.HeadshotMultiplier    = 2.75
 SWEP.Primary.Spread		  = .02	
 SWEP.Primary.IronAccuracy = .01 
 
